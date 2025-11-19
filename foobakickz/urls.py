6d10a4a0abed306f3f18092e34e7f24d82cc130c
cheraynasya <cheryl.raynasya@cs.ui.ac.id>
2025-11-19 11:55:25 +0700

    Tugas 9

--- foobakickz/urls.py
+++ foobakickz/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main.views import show_main
 
 
 urlpatterns = [
+    path('', show_main, name='main_page'),
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('main/', include('main.urls')),
     path('auth/', include('authentication.urls')),
 ]
